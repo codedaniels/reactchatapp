9a9759ec170c2730617a37a2b710ec639e86e5f7
Daniel S <danielksstone@gmail.com>
2018-10-24 14:44:05 -0700

    message list js file partially complete

---
+++ components/MessageList.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const DUMMY_DATA = [
+    {
+        senderId: 'daniel',
+        text: 'Hey, how is it going?'
+    },
+    {
+        senderId: 'janedoe',
+        text: 'Great!  How about you?'
+    },
+    {
+        senderId: 'daniel',
+        text: 'Good to hear! I am great as well'
+    }
+]
+class MessageList extends React.Component {
+    render() {
+        return (
+            <div className="message-list">
+                {DUMMY_DATA.map((message, index) => {
+                    return (
+                        <div>{message.text}</div>
+                    )
+                })
+                    /* <div className="help-text">MessageList</div> */}
+            </div>
+        )
+    }
+}

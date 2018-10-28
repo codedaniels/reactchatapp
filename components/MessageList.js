e45b94ac5742c47d5cfeebc51c9f155f1897b86d
Daniel S <danielksstone@gmail.com>
2018-10-28 16:23:00 -0700

    newroomform js

--- components/MessageList.js
+++ components/MessageList.js
@@ -1,31 +1,38 @@
 import React from 'react'
-
-const DUMMY_DATA = [
-    {
-        senderId: 'daniel',
-        text: 'Hey, how is it going?'
-    },
-    {
-        senderId: 'janedoe',
-        text: 'Great!  How about you?'
-    },
-    {
-        senderId: 'daniel',
-        text: 'Good to hear! I am great as well'
-    }
-]
+import ReactDOM from 'react-dom'
+import Message from './Message'
 
 class MessageList extends React.Component {
+    componentWillUpdate() {
+        const node = ReactDOM.findDOMNode(this);
+        this.shouldScrollBottom = node.scrollTop + node.clientHeight + 50 >= node.scrollHeight;
+    }
+
+    componentDidUpdate() {
+        if (this.shouldScrollBottom) {
+            const node = ReactDOM.findDOMNode(this);
+            node.scrollTop = node.scrollHeight
+        }
+    }
+        
     render() {
+        if (!this.props.currentRoomId) {
+            return ( 
+                <div className="message-list">
+                    <div className="join-room">
+                        &larr; Join a room!
+                    </div>
+                </div>
+            )
+        } 
         return (
             <div className="message-list">
-                {DUMMY_DATA.map((message, index) => {
-                    return (
-                        <div>{message.text}</div>
-                    )
-                })
-                    /* <div className="help-text">MessageList</div> */}
+                {this.props.messages.map((message, index) => {
+                    return <Message key={index} user={message.senderId} text={message.text} /> 
+                })}
             </div>
         )
     }
 }
+
+export default MessageList

71b790f440688dd70b0c7f99985ddf4c66940895
Daniel S <danielksstone@gmail.com>
2018-10-30 13:34:41 -0700

    messagelist updated

--- components/MessageList.js
+++ components/MessageList.js
@@ -2,34 +2,54 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Message from './Message'
 
-class MessageList extends React.Component {
-    componentWillUpdate() {
-        const node = ReactDOM.findDOMNode(this);
-        this.shouldScrollBottom = node.scrollTop + node.clientHeight + 50 >= node.scrollHeight;
-    }
 
-    componentDidUpdate() {
-        if (this.shouldScrollBottom) {
-            const node = ReactDOM.findDOMNode(this);
-            node.scrollTop = node.scrollHeight
-        }
+const DUMMY_DATA = [
+    {
+        senderId: 'perborgen',
+        text: 'Hey, how is it going?'
+    },
+    {
+        senderId: 'janedoe',
+        text: 'Great! How about you?'
+    },
+    {
+        senderId: 'perborgen',
+        text: 'Good to hear! I am great as well'
     }
+]
+
+
+class MessageList extends React.Component {
+    // componentWillUpdate() {
+    //     const node = ReactDOM.findDOMNode(this);
+    //     this.shouldScrollBottom = node.scrollTop + node.clientHeight + 50 >= node.scrollHeight;
+    // }
+
+    // componentDidUpdate() {
+    //     if (this.shouldScrollBottom) {
+    //         const node = ReactDOM.findDOMNode(this);
+    //         node.scrollTop = node.scrollHeight
+    //     }
+    // }
         
     render() {
-        if (!this.props.currentRoomId) {
-            return ( 
-                <div className="message-list">
-                    <div className="join-room">
-                        &larr; Join a room!
-                    </div>
-                </div>
-            )
-        } 
+        // if (!this.props.currentRoomId) {
+        //     return ( 
+        //         <div className="message-list">
+        //             <div className="join-room">
+        //                 &larr; Join a room!
+        //             </div>
+        //         </div>
+        //     )
+        // } 
         return (
             <div className="message-list">
-                {this.props.messages.map((message, index) => {
+                {
+                    
+                    
+                    /* {this.props.messages.map((message, index) => {
                     return <Message key={index} user={message.senderId} text={message.text} /> 
-                })}
+                })} */}
             </div>
         )
     }
